ae892001dbacb041672c2713dbcaf22d3a14e903
tso1158687 <101104044@nccu.edu.tw>
2017-03-17 15:50:52 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -11,30 +11,15 @@
     function ToBuyController(ShoppingListCheckOffService) {
         var toBuy = this;
         toBuy.items = ShoppingListCheckOffService.getToBuyItems();
-       
-        toBuy.state = ShoppingListCheckOffService.state;
-        toBuy.state2 = ShoppingListCheckOffService.state2;
+
+        toBuy.state = ShoppingListCheckOffService.buyListState();
+
         toBuy.button = function(itemIndex) {
-            
+
             ShoppingListCheckOffService.button(itemIndex);
-            console.log("service:" + ShoppingListCheckOffService.state2)
 
-            var buyState;
-            buyState = function() {
 
-                if (toBuy.items.length === 0) {
-                    toBuy.state = true;
-                    ShoppingListCheckOffService.state2 = false;
-                } else {
-                    toBuy.state = false;
-                    ShoppingListCheckOffService.state2 = false;
-                };
-                console.log("state:" + toBuy.state);
-                console.log("state2:" + toBuy.state2);
-                console.log("service:" + ShoppingListCheckOffService.state2);
 
-            };
-            buyState();
 
 
         };
@@ -46,9 +31,7 @@
     function AlreadyBoughtController(ShoppingListCheckOffService) {
         var alreadyBought = this;
         alreadyBought.items = ShoppingListCheckOffService.getBoughtItems();
-        alreadyBought.state = ShoppingListCheckOffService.state2;
-
-        console.log("alreadyBought state:" + alreadyBought.state);
+        alreadyBought.state = ShoppingListCheckOffService.boughtListState();
 
 
     };
@@ -96,26 +79,12 @@
             toBuyItems.splice(itemIndex, 1);
 
         };
+        service.buyListState = function() {
+            return toBuyItems;
+        };
 
-        service.state = false;
-        service.state2 = true;
-
-       
-        service.buyState = function() {
-            if (toBuyItems.length === 0) {
-                console.log("true");
-                service.state = true;
-                service.state2 = false;
-
-            } else {
-                console.log(service.state);
-                service.state = true;
-                service.state2 = false;
-                console.log("false");
-
-            };
-
-
+        service.boughtListState = function() {
+            return boughtItems;
         };
 
     }
